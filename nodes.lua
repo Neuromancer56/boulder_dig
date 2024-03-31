245bdc5942493442ded070a7ec5466aee7865d65
Neuromancer56 <neuromancer56@gmail.com>
2024-03-30 20:44:17 -0500

    got boulders working

--- nodes.lua
+++ nodes.lua
@@ -1,3 +1,66 @@
+--manual changes: 
+--E:\games\minetest-5.8.0-win64\games\minetest_game\mods\default\mapgen.lua L:7  minetest.register_alias("mapgen_stone", "default:stone") > minetest.register_alias("mapgen_stone", "default:dirt")
+--E:\games\minetest-5.8.0-win64\mods\falling_item\init.lua  local L:3 function add_fall_damage(node, damage) > function add_fall_damage(node, damage)
+--enable wielded light & give yourself a torch.
+--[[minetest.override_item("default:gravel", {
+    groups = {
+        floored = 1,
+        -- Keep any other existing groups
+        crumbly = 2,
+        falling_node = 1,
+        -- etc.
+    }
+})]]
+
+--[[minetest.register_biome({
+    name = "boulder",
+    node_top = "default:dirt",
+    depth_top = 1,
+    node_filler = "default:dirt",
+    depth_filler = 3,
+    node_riverbed = "swamp:mud",
+    depth_riverbed = 2,
+    node_dungeon = "swamp:mud_brick",
+    node_dungeon_alt = "swamp:mud_block",
+    y_max = 10,
+    y_min = -20,
+    heat_point = 80,
+    humidity_point = 98,
+})]]
+
+
+
+function default.node_sound_boulder_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name = "default_hard_footstep", gain = 0.25}
+	table.dig = table.dig or
+			{name = "default_dig_cracky", gain = 0.35}
+	table.dug = table.dug or
+			{name = "default_hard_footstep", gain = 1.0}
+	table.place = table.place or
+			{name = "default_dug_node", gain = 1.0}
+			--{name = "default_place_node", gain = 1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+
+minetest.register_node("boulder_dig:boulder", {
+	description = "Boulder",
+	tiles = {"default_gravel.png"},
+	groups = {cracky = 2, falling_node = 1, falling_node_hurt =1},
+	sounds = default.node_sound_boulder_defaults(),
+	--sounds = default.node_sound_gravel_defaults(),
+	drop = {
+		max_items = 1,
+		items = {
+			{items = {"default:gravel"}}
+		}
+	}
+})
+
+--default_dug_node.1, default_dig_choppy.3
 
 local function dirtTouchAction(player)
 	local pos = player:get_pos()
@@ -6,9 +69,7 @@ local function dirtTouchAction(player)
 
 local yaw = player:get_look_horizontal()
 local yaw_degrees = (yaw + 2 * math.pi) % (2 * math.pi) * 180 / math.pi
-minetest.log("x","yaw_degrees:"..yaw_degrees)	
-
-
+--minetest.log("x","yaw_degrees:"..yaw_degrees)	
 
 local x_start = 0
 local x_end = 0
@@ -34,26 +95,54 @@ end
 
 --minetest.log("x","x_start:"..x_start..", x_end:"..x_end)	
 --minetest.log("x","z_start:"..z_start..", z_end:"..z_end)	
-	
+local dirt_dug = false
+
 	for dx = x_start, x_end do
-		minetest.log("x","dx:"..dx)	
-		for dy = -1, 1 do
+		--minetest.log("x","dx:"..dx)	
+		for dy = 0, 1 do
 			for dz = z_start, z_end do
-				minetest.log("x","dz:"..dz)	
+				--minetest.log("x","dz:"..dz)	
 				local neighbor_pos = {x = pos.x + dx, y = pos.y + dy, z = pos.z + dz}
 				local node = minetest.get_node(neighbor_pos)
 				
 				if node.name == "default:dirt" then
-					minetest.swap_node(neighbor_pos, {name = "air", param2 = node.param2})
+					minetest.set_node(neighbor_pos, {name = "air", param2 = node.param2})
+					dirt_dug = true
 							
 				end
 			end
 		end
 	end
-    minetest.sound_play("default_break_glass", {pos = pos, gain = 0.5, max_hear_distance = 10})
+    if dirt_dug then 
+		minetest.sound_play("default_dig_crumbly", {pos = pos, gain = 0.5, max_hear_distance = 10}) 
+		
+	for dx = x_start, x_end do
+		--minetest.log("x","dx:"..dx)	
+		for dy = 1, 2 do
+			for dz = z_start, z_end do
+				--minetest.log("x","dz:"..dz)	
+				local neighbor_pos = {x = pos.x + dx, y = pos.y + dy, z = pos.z + dz}
+				local node = minetest.get_node(neighbor_pos)
+				minetest.check_for_falling(neighbor_pos)
+			end
+		end
+	end
+	end
 		--minetest.log("x","x:"..neighbor_pos.x ..",z:"..neighbor_pos.z)		
+	
 end
 
 -- Register global step action for magma nodes
 registerNodeTouchAction("default:dirt", dirtTouchAction)
+add_fall_damage("boulder_dig:boulder", 3)
 
+minetest.register_ore({
+    ore_type = "scatter",
+    ore = "boulder_dig:boulder",
+    wherein = "default:dirt",
+    clust_scarcity = 4 * 4 * 4,
+    clust_num_ores = 8,
+    clust_size = 3,
+    height_min = 1,
+    height_max = 31,
+})
